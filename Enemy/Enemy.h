bc64ec22d9f9f3559b9376f40c9c688ccf541c94
REO MIYA <41591rionmiyazaki@gmail.com>
2022-09-01 15:00:58 +0900

    03_06完成

--- Enemy/Enemy.h
+++ Enemy/Enemy.h
@@ -1,16 +1,30 @@
 #pragma once
 
 #include "Model.h"
-#include "MyFunc.h"
+#include "MyFunc/MyFunc.h"
 #include "WorldTransform.h"
 #include <cassert>
 #include <random>
 
 class Enemy
 {
+	enum class Phase
+	{
+		None,
+		Approach,	//接近する
+		Leave,		//離脱する
+	};
+
 public:
+	// 初期化
 	void Initialize(Model* model, uint32_t textureHandle);
+	// 接近
+	void Approach();
+	// 離脱
+	void Leave();
+	// 更新
 	void UpDate();
+	// 描画
 	void Draw(ViewProjection viewProjection);
 
 private:
@@ -20,5 +34,8 @@ private:
 	Model* model_ = nullptr;
 	// テクスチャハンドル
 	uint32_t textureHandle_ = 0u;
+
+	//敵の行動パターン
+	Phase phase_ = Phase::None;
 
 };
